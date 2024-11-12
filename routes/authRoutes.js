e61731117c395ef0c86d54bed29c1c87c1ad78d6
hschoefl <harald.schoefl@a1.at>
2024-11-12 13:36:08 +0100

    working now with axios

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,52 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const axios = require("axios");
+const axios = require('axios');
 
 const TV_PORT = 1516;
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const tvIP = req.tvIP; // added by middleware to the body
 
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Accept", "application/json");
-  myHeaders.append("Cache-Control", "no-cache");
-  myHeaders.append("Access-Control-Allow-Origin", "*");
-  myHeaders.append("Connection", "keep-alive");
-
   const accessTokenRequest = {
-    jsonrpc: "2.0",
-    method: "createAccessToken",
-    id: "1",
+    jsonrpc: '2.0',
+    method: 'createAccessToken',
+    id: '1',
   };
 
-  // accessTokenRequest wird an den TV geschickt
   const urlSend = `https://${tvIP}:${TV_PORT}`;
-  console.log(urlSend);
 
-  const response = await fetch(urlSend, {
-    method: "POST",
-    mode: "no-cors",
-    headers: myHeaders,
-    body: JSON.stringify(accessTokenRequest),
-  });
+  const response = await axios.post(urlSend, accessTokenRequest);
 
-  if (response.ok) {
-    console.log("Response was ok.");
+  if (response.statusText !== 'OK') {
+    throw new Error(`Can not get access token from TV with IP ${tvIP}`);
+  } else {
+    res.status(200).json(response.data);
   }
-  // console.log(response);
-  if (!response.ok) {
-    console.log("Response was not ok.", response.statusText);
-    console.log("Response Type:", response.type);
-    console.log("Content-Type: ", response.headers.get("content-type"));
-    console.log("Content-Length: ", response.headers.get("content-length"));
-  }
-
-  const data = await response.json();
-
-  // console.log(data);
-
-  res.status(200).json({ message: "nur ein test" });
 });
 
 module.exports = router;
--- routes/keyRoutes.js
+++ routes/keyRoutes.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.post("/:keyName", (req, res) => {
+router.post('/:keyName', (req, res) => {
   const remoteKey = req.params.keyName;
 
-  console.log(req.tvIP); // inserted throug middleware
+  console.log(req.tvIP); // inserted by middleware
   console.log(remoteKey);
 
   const pressedKey = {
-    jsonrpc: "2.0",
-    method: "remoteKeyControl",
+    jsonrpc: '2.0',
+    method: 'remoteKeyControl',
     params: {
       remoteKey,
-      AccessToken: "519fcc9b8c4e4b64b5531c7281bf700a",
+      AccessToken: '519fcc9b8c4e4b64b5531c7281bf700a',
     },
-    id: "1",
+    id: '1',
   };
 
   // pressed key wird an den TV geschickt
